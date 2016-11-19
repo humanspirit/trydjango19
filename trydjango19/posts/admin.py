cbbd53b3302e6585b12b7e38bacddd98e24d8eed
humanspirit <humanspirit@posteo.de>
2016-11-20 00:58:46 +0100

    Admin customizing

--- trydjango19/posts/admin.py
+++ trydjango19/posts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ["title","update", "timestamp"]
+    class Meta:
+        model = Post
+
+admin.site.register(Post, PostModelAdmin)
