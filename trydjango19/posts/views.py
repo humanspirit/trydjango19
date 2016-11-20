44fc0f727d08c49b5f5b0751d494258d384477b2
humanspirit <humanspirit@posteo.de>
2016-11-20 17:31:29 +0100

    template context

--- trydjango19/posts/views.py
+++ trydjango19/posts/views.py
@@ -11,7 +11,16 @@ def post_detail(request):
     return HttpResponse("<h1>Hello Detail </h1>")
 
 def post_list(request):
-    return HttpResponse("<h1>Hello List </h1>")
+    if request.user.is_authenticated():
+        context_data = {
+            "title": "My List"
+        }
+    else:
+        context_data = {
+            "title": "List"
+        }
+    return render(request, "index.html", context_data)
+
 
 def post_update(request):
     return HttpResponse("<h1>Hello Update </h1>")
